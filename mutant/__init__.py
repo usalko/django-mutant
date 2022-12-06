579dc2a2ee9d47789ab5e2feae16f86486f2bdf1
Vanya U <ivict@rambler.ru>
2022-12-07 00:08:31 +0300

    # There is the fix for the error:
      --  Collecting git+https://github.com/usalko/django-mutant.git@feature/django4 (from -r /requirements.txt (line 5))
      Cloning https://github.com/usalko/django-mutant.git (to revision feature/django4) to /tmp/pip-req-build-1551ecgm
      Running command git clone --filter=blob:none --quiet https://github.com/usalko/django-mutant.git /tmp/pip-req-build-1551ecgm
      Running command git checkout -b feature/django4 --track origin/feature/django4
      branch 'feature/django4' set up to track 'origin/feature/django4'.
      Switched to a new branch 'feature/django4'
      Resolved https://github.com/usalko/django-mutant.git to commit 99031ed21d7e06366e8a2fec1a66b81afed1e35d
      Preparing metadata (setup.py): started
      Preparing metadata (setup.py): finished with status 'error'
      error: subprocess-exited-with-error
    
      × python setup.py egg_info did not run successfully.
      │ exit code: 1
      ╰─> [8 lines of output]
          Traceback (most recent call last):
            File "<string>", line 2, in <module>
            File "<pip-setuptools-caller>", line 34, in <module>
            File "/tmp/pip-req-build-1551ecgm/setup.py", line 5, in <module>
              from mutant import __version__
            File "/tmp/pip-req-build-1551ecgm/mutant/__init__.py", line 5, in <module>
              from django.utils.version import get_version
          ModuleNotFoundError: No module named 'django'
          [end of output]
    
      note: This error originates from a subprocess, and is likely not a problem with pip.

--- mutant/__init__.py
+++ mutant/__init__.py
@@ -2,11 +2,11 @@ from __future__ import unicode_literals
 
 import logging
 
-from django.utils.version import get_version
+# from django.utils.version import get_version
 
 VERSION = (0, 3, 0, 'alpha', 5)
 
-__version__ = get_version(VERSION)
+__version__ = '.'.join(VERSION)
 
 logger = logging.getLogger('mutant')
 
